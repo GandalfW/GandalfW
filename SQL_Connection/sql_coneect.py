ed258a59fd89205221f8f6ba9b811bd1cca18a60
GandalfW <itdoctor@outlook.com>
2021-08-16 15:01:48 -0500

    SQL Seach Study Case

---
+++ SQL_Connection/sql_coneect.py
@@ -0,0 +1,25 @@
+import mysql.connector
+import difflib
+
+con = mysql.connector.connect(
+    user="ardit700_student",
+    password="ardit700_student",
+    host="108.167.140.122",
+    database="ardit700_pm1database"
+)
+
+cursor = con.cursor()
+word = ""
+while (True):
+    word = input("Enter a word: ")
+    word = word.lower()
+    
+    query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word)
+    results = cursor.fetchall()
+    if results:
+        for p in results:
+            print(p)
+    elif word == "e":
+        break
+    else:
+        print("The word don't exist, please write other one")
