b420c92685911f171ce0d8b2394826b786679386
GandalfW <itdoctor@outlook.com>
2021-08-16 15:20:33 -0500

    Correction of the exercise SQL query

--- SQL_Connection/sql_coneect.py
+++ SQL_Connection/sql_coneect.py
@@ -13,13 +13,12 @@ word = ""
 while (True):
     word = input("Enter a word: ")
     word = word.lower()
-    
     query = cursor.execute("SELECT * FROM Dictionary WHERE Expression = '%s'" % word)
     results = cursor.fetchall()
     if results:
         for p in results:
             print(p)
-    elif word == "e":
+    elif word == "ex-1":
         break
     else:
         print("The word don't exist, please write other one")
